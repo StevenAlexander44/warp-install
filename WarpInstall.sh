ac1f9c1b81e5de570f5a545dcb1399d74f898d87
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2021-12-08 12:53:43 -0500

    Update WarpInstall.sh

--- WarpInstall.sh
+++ WarpInstall.sh
@@ -1,6 +1,10 @@
 sudo apt update
 curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
 osVersion=`dpkg --status tzdata|grep Provides|cut -f2 -d'-'`
+case $osVersion in
+    focal|xenial|bullseye|buster|sketch) echo good to go;;
+    *)    echo choose from focal, xenial, bullseye, buster, or sketch; read osVersion;;
+esac
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ '"$osVersion"' main' | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
 sudo apt update
 sudo apt install cloudflare-warp -y
