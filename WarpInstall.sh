e1f92fc9a9ea5330a6f81b52cb79a0512b2c4ca3
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2022-01-08 21:35:51 -0500

    Update WarpInstall.sh

--- WarpInstall.sh
+++ WarpInstall.sh
@@ -1,10 +1,13 @@
 sudo apt update
 curl https://pkg.cloudflareclient.com/pubkey.gpg | sudo gpg --yes --dearmor --output /usr/share/keyrings/cloudflare-warp-archive-keyring.gpg
-osVersion=`dpkg --status tzdata|grep Provides|cut -f2 -d'-'`
+osVersion=`lsb_release -cs`
 case $osVersion in
-    focal|xenial|bullseye|buster|sketch) echo good to go;;
+    focal|xenial|bullseye|buster|sketch) echo $osVersion is good to go;;
     *)    echo choose from focal, xenial, bullseye, buster, or sketch; read osVersion;;
 esac
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/cloudflare-warp-archive-keyring.gpg] https://pkg.cloudflareclient.com/ '"$osVersion"' main' | sudo tee /etc/apt/sources.list.d/cloudflare-client.list
 sudo apt update
 sudo apt install cloudflare-warp -y
+echo;echo;echo
+echo now you can use 'warp-cli register' to register \(only required once\)
+echo and use 'warp-cli connect' to connect
