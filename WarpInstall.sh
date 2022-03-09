96228d5e0747e53749ef0e24d9baec0fb6d57d1e
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2022-03-09 19:17:14 +0000

    Update WarpInstall.sh

--- WarpInstall.sh
+++ WarpInstall.sh
@@ -1,4 +1,4 @@
-sudo apt install wireguard openresolv
+sudo apt install wireguard openresolv -y
 curl -Lo wgcf $(curl -s https://api.github.com/repos/ViRb3/wgcf/releases/latest | grep "https.*linux_amd64" | cut -d : -f 2,3 | tr -d \")
 chmod u+x wgcf
 sudo mv wgcf /bin
