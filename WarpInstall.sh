c937ad95fbe31cf37ede4f5cd17f104803a45ec2
StevenAlexander44 <64290624+StevenAlexander44@users.noreply.github.com>
2022-03-09 20:30:09 -0500

    Update WarpInstall.sh

--- WarpInstall.sh
+++ WarpInstall.sh
@@ -3,7 +3,7 @@ curl -Lo wgcf $(curl -s https://api.github.com/repos/ViRb3/wgcf/releases/latest 
 chmod u+x wgcf
 sudo mv wgcf /bin
 sudo -u root sh -c 'cd /etc/wireguard; printf "\n" | wgcf register && wgcf generate' &&
-sudo mv /etc/wireguard/wgcf-profile.conf /etc/wireguard/warp.conf &&
-wg-quick up warp &&
-wg-quick down warp &&
+sudo mv /etc/wireguard/wgcf-profile.conf /etc/wireguard/warp.conf
+wg-quick up warp
+wg-quick down warp
 echo wg-quick up warp
